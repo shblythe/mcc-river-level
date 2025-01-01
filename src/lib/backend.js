f6186e5dc39f2bb085005645e5b690fd4e93d7bf
Stephen Blythe <stephen@urwick.co.uk>
2025-01-01 12:24:18 +0000

    Use vps-hosted cors proxy
    Hopefully a temporary measure - government website is returning two cors
    headers for some reason, which is breaking the app.

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -1,7 +1,7 @@
 const host='https://environment.data.gov.uk/flood-monitoring'
 
 export function backendFetch(uri) {
-    return fetch(`${host}/${uri}`)
+    return fetch(`http://vps.urwick.co.uk:8888/${host}/${uri}`, { })
         .then(response => response.json())
         .catch(() => {throw new Error(`backendFetch failed: ${uri}`)});
 }
