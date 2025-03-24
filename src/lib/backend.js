6901e4b4b607c4a112c50a8a898c075634274188
Stephen Blythe <stephen@urwick.co.uk>
2025-03-24 19:58:23 +0000

    Don't seem to need cors any more

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -1,7 +1,9 @@
+//const cors='https://vps.urwick.co.uk:8888/'
+const cors=''
 const host='https://environment.data.gov.uk/flood-monitoring'
 
 export function backendFetch(uri) {
-    return fetch(`https://vps.urwick.co.uk:8888/${host}/${uri}`, { })
+    return fetch(`${cors}${host}/${uri}`, { })
         .then(response => response.json())
         .catch(() => {throw new Error(`backendFetch failed: ${uri}`)});
 }
